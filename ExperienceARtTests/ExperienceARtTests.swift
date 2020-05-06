3b4afa1a2990101f015581aff736372ff2bfe7dd
Teresa Lasarow <teresa.lasarow@student.hpi.de>
2020-05-06 09:06:28 +0200

    Initial Commit

---
+++ ExperienceARtTests/ExperienceARtTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExperienceARtTests.swift
+//  ExperienceARtTests
+//
+//  Created by Teresa Lasarow on 06.05.20.
+//  Copyright © 2020 Teresa Lasarow. All rights reserved.
+//
+
+import XCTest
+@testable import ExperienceARt
+
+class ExperienceARtTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
